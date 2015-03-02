452dd28366d492741bf2c8cdf0ed455631fd54e5
andforce <86118@163.com>
2015-03-02 15:46:03 +0800

    add print log

---
+++ src/org/zarroboogs/devutils/OutLog.java
@@ -0,0 +1,33 @@
+
+package org.zarroboogs.devutils;
+
+import android.util.Log;
+
+public class OutLog {
+    private static void d(String tag, String msg) {
+        if (msg.trim().contains("\n")) {
+            String[] lineStrings = msg.split("\n");
+            for (String string : lineStrings) {
+                Log.d(tag, string);
+            }
+        } else {
+            Log.d(tag, msg);
+        }
+
+    }
+
+    public static void print(String tag, String msg) {
+        if (msg.length() > 3000) {
+            int len = msg.length();
+            int devideNumber = len / 3000;
+            int j = 0;
+            for (int i = 0; i < devideNumber; i++) {
+                d(tag, msg.substring(j, j + 3000));
+                j += 3000;
+            }
+            d(tag, msg.substring(j, msg.length()));
+        } else {
+            d(tag, msg);
+        }
+    }
+}
